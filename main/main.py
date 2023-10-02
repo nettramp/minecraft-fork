58a73d71fcf70070798a56154a45f2443b13ade4
Сергей Еремин <45639042+nettramp@users.noreply.github.com>
2023-10-02 21:02:59 +0300

    создаем объекты

--- main/main.py
+++ main/main.py
@@ -3,5 +3,10 @@ from ursina import *
 app = Ursina() 
 
 # здесь будет описана игровая логика
+# создаем объекты модели cube, с текстурой white_cube и заданными координатами
+for x in range(16): 
+   for z in range(16):
+       Entity(model="cube", texture="white_cube", position=Vec3(x,0,z))
+
 
 app.run()
