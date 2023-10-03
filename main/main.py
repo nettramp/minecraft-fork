506aee989f0ad090db38c6b7c856fe20f1f1683a
Сергей Еремин <45639042+nettramp@users.noreply.github.com>
2023-10-03 19:52:32 +0300

    добавили текстуры и руку

--- main/main.py
+++ main/main.py
@@ -1,4 +1,6 @@
 from ursina import *
+# импортируем объект персонажа
+from ursina.prefabs.first_person_controller import FirstPersonController
 
 app = Ursina() 
 
@@ -7,6 +9,17 @@ app = Ursina()
 for x in range(16): 
    for z in range(16):
        Entity(model="cube", texture="white_cube", position=Vec3(x,0,z))
+# создаем персонажа
+player = FirstPersonController() 
+# активируем невесомость, чтобы персонаж не упал в пустоту
+player.gravity = 0.0 
 
+# загружаем текстуру руки
+arm_texture = load_texture('assets/arm_texture.png')
 
+# объявляем объект hand, привязываем к камере camera.ui, загружаем модель и размещаем ее в правом нижнем углу 
+hand = Entity(parent = camera.ui, model = 'assets/arm',
+             texture = arm_texture, scale = 0.2,
+             rotation = Vec3(150, -10,0), position = Vec2(0.5,-0.6))
+...
 app.run()
